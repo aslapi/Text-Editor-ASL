41e3f9fcf7e4238037a5359f3eb25caae9dab565
Alex LaPierre <aslapierre7@gmail.com>
2024-07-15 16:45:23 -0500

    fixing image loading issue

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,17 +1,85 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const path = require('path');
+// const { InjectManifest } = require('workbox-webpack-plugin');
+
+// module.exports = () => {
+//   return {
+//     mode: 'development',
+//     entry: {
+//       main: './src/js/index.js',
+//       install: './src/js/install.js'
+//     },
+//     output: {
+//       filename: '[name].bundle.js',
+//       path: path.resolve(__dirname, 'dist'),
+//     },
+//     plugins: [
+//       new HtmlWebpackPlugin({
+//         template: './index.html',
+//         title: 'JATE',
+//       }),
+//       new WebpackPwaManifest({
+//         name: 'JATE',
+//         short_name: 'JATE',
+//         description: 'Just Another Text Editor',
+//         background_color: '#ffffff',
+//         theme_color: '#ffffff',
+//         start_url: './',
+//         publicPath: './',
+//         icons: [
+//           {
+//             src: path.resolve('src/images/logo.png'),
+//             sizes: [96, 128, 192, 256, 384, 512],
+//             destination: path.join('assets', 'icons'),
+//             purpose: 'any maskable',
+//           },
+//         ],
+//       }),
+//       new InjectManifest({
+//         swSrc: './src-sw.js',
+//         swDest: 'service-worker.js'
+//       }),
+//     ],
+//     module: {
+//       rules: [
+//         {
+//           test: /\.css$/i,
+//           use: ['style-loader', 'css-loader'],
+//         },
+//         {
+//           test: /\.m?js$/,
+//           exclude: /(node_modules|bower_components)/,
+//           use: {
+//             loader: 'babel-loader',
+//             options: {
+//               presets: ['@babel/preset-env'],
+//             },
+//           },
+//         },
+//         {
+//           test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//           type: 'asset/resource',
+//           generator: {
+//             filename: 'assets/icons/[name].hash[ext]',
+//           },
+//         },
+//       ],
+//     },
+//   };
+// };
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -19,25 +87,36 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
+        template: './index.html', // Ensure this path is correct
         title: 'JATE',
       }),
-      new WebpackPwaManifest(
-        // CREATE manifest.json??
-        // 
-      ),
-      new InjectManifest({
-        swSrc: './src-sw.js'
+      new WebpackPwaManifest({
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+            purpose: 'any maskable',
+          },
+        ],
       }),
-
-      
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
     ],
-
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader']
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
@@ -48,6 +127,10 @@ module.exports = () => {
               presets: ['@babel/preset-env'],
             },
           },
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
         },
       ],
     },
